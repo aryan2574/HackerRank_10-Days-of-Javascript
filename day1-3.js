d40091069897c2976e040e58f1960c8381c14fa3
Ayush Aryan <ayush.aryan94@yahoo.com>
2020-05-23 13:55:20 -0400

    Day 1: Let and Const

---
+++ day1-3.js
@@ -0,0 +1,13 @@
+const PI = 3.141592653589793238
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    var r = readLine()
+   
+   // Print the area of the circle:
+    var area = PI * r * r
+    console.log(area);
+   
+   // Print the perimeter of the circle:
+    var perimeter = 2 * PI * r
+    console.log(perimeter);
